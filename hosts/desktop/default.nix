cea98a4856cc3a3695b1b6dad3aeab1eb88e999e
Taurforod <dragonkostya@gmail.com>
2026-09-10 01:10:37 +0200

    feat(desktop): configure mounts for games, Data and Storage drives

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -15,6 +15,44 @@
   # Use latest kernel.
   boot.kernelPackages = pkgs-unstable.linuxPackages_zen;
 
+  fileSystems."/mnt/games" = {
+    device = "/dev/disk/by-uuid/e38eb377-fbdb-4692-a992-442ceabed2d4";
+    fsType = "btrfs";
+    options = [
+      "rw"
+      "noatime"
+      "compress=zstd:3"
+      "autodefrag"
+      "discard=async"
+    ];
+  };
+
+  fileSystems."/mnt/Data" = {
+    device = "/dev/disk/by-uuid/091C3BAA320C467E";
+    fsType = "ntfs-3g";
+    options = [
+      "rw"
+      "nofail"
+      "uid=1000"
+      "gid=100"
+      "dmask=022"
+      "fmask=113"
+    ];
+  };
+
+  fileSystems."/mnt/Storage" = {
+    device = "/dev/disk/by-uuid/664093E54093BA6F";
+    fsType = "ntfs-3g";
+    options = [
+      "rw"
+      "nofail"
+      "uid=1000"
+      "gid=100"
+      "dmask=022"
+      "fmask=113"
+    ];
+  };
+
   networking.hostName = "desktop"; # Define your hostname.
 
   system.stateVersion = "26.05";
